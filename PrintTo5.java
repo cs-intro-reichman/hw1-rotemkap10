b2019c041fae9b261d9fc8bab34c6d8cb2a1ff28
Rotem Kaplan <RotemKaplan@Rotem-Kaplans-Mackbook.local>
2024-11-04 19:09:17 +0200

    complited part 1 inside Demo0 file

--- PrintTo5.java
+++ PrintTo5.java
@@ -4,6 +4,7 @@ public class PrintTo5 {
 	public static void main(String[] args) {
 		// Declares an int variable, and sets it to 1
         int i = 0;
+        int j = 0;
         // Complete the program's code here:
     }
  }
