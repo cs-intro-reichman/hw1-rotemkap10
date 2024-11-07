7711aafcab3dfa71ecf01f6f48e77ae8412e9f3c
Rotem Kaplan <RotemKaplan@Rotem-Kaplans-Mackbook.local>
2024-11-07 16:49:28 +0200

    uploaded the last exrecise

--- TimeFormat.java
+++ TimeFormat.java
@@ -12,7 +12,20 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+		if(hours>12){System.out.println(hours-12 + ":" + args[0].charAt(3) + args[0].charAt(4) + " " + "PM");}
+		else if(hours ==12){System.out.println(hours + ":" + args[0].charAt(3) + args[0].charAt(4) + " " + "PM");}
+		else
+			{
+				if(minutes>9){System.out.println(hours + ":" + args[0].charAt(3) + args[0].charAt(4) + " " + "AM");}
+				else{System.out.println(args[0].charAt(1) + ":" + args[0].charAt(3) + args[0].charAt(4) + " " + "AM");}
+			}
+		
+			
 
+	
+
+
+	
         
 
 	}
